c9a758b2b02673b82b9f4b32ac9a330a10305a05
Stefan Mandel <mandelst@gmail.com>
2025-06-13 12:34:56 +0200

    make with devices @Testable

--- src/main/java/net/amygdalum/allotropy/WithDevices.java
+++ src/main/java/net/amygdalum/allotropy/WithDevices.java
@@ -1,5 +1,7 @@
 package net.amygdalum.allotropy;
 
+import org.junit.platform.commons.annotation.Testable;
+
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
@@ -9,6 +11,7 @@ import java.lang.annotation.Target;
 
 @Retention(RUNTIME)
 @Target({TYPE, METHOD})
+@Testable
 public @interface WithDevices {
 
     WithDevice[] value();
